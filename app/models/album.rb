17edb418215159420dc70e59bbde6a8a376707f0
condef5 <davis.con.fab@gmail.com>
2019-04-12 16:47:41 -0500

    Refactor Album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
   has_and_belongs_to_many :songs, join_table: 'associations'
   has_and_belongs_to_many :artists, join_table: 'associations'
-
-  validates :title, presence: true, lenght: {minimum: 1, maximum: 50}
-  validates :rating, presence: false
+  
+  validates :title, presence: true
+  validates :rating, presence: true, acceptance: { accept: [-1, 1], message: 'must be 1 or -1' }
 end
