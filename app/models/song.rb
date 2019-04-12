4d6101908bb071b0cce5b4336ebd86f932fd29c7
Rixele <liamrn94@gmail.com>
2019-04-12 09:42:27 -0500

    Add validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,9 @@
 class Song < ApplicationRecord
   has_and_belongs_to_many :albums, join_table: 'associations'
   has_and_belongs_to_many :artists, join_table: 'associations'
+
+  validates :title, presence: true, length: {minimum: 1, maximum: 50}
+  validates :duration, presence: false
+  validates :rating, presence: false
+  validates :progress, presence: false
 end
